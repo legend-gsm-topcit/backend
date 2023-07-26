00c664c0d0034dd7af6d9cdcd392dd988aa1836c
최장우 <jangwoo-choi@choejang-uui-MacBookAir.local>
2023-07-26 13:53:02 +0900

    Init

---
+++ src/main/java/com/example/cocoblue/CocoblueApplication.java
@@ -0,0 +1,13 @@
+package com.example.cocoblue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CocoblueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CocoblueApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cocoblue/CocoblueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cocoblue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CocoblueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
