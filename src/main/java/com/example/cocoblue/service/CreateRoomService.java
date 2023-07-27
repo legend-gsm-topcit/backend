daf858482cd58b2b204c5c365fa76b43d6c11736
jangwooooo <s22018@gsm.hs.kr>
2023-07-27 23:21:48 +0900

    Add create and join logging

--- src/main/java/com/example/cocoblue/service/CreateRoomService.java
+++ src/main/java/com/example/cocoblue/service/CreateRoomService.java
@@ -5,12 +5,14 @@ import com.example.cocoblue.domain.Room;
 import com.example.cocoblue.domain.Member;
 import com.example.cocoblue.repository.RoomRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
 import java.util.UUID;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class CreateRoomService {
 
@@ -34,6 +36,8 @@ public class CreateRoomService {
                 .answerMatched(false)
                 .hasDrawn(false)
                 .build();
+
+        log.info(roomId.toString(), name);
 
         room.getMembers().put(name, member);
         roomRepository.setRoom(roomId, room);
--- src/main/java/com/example/cocoblue/service/JoinRoomService.java
+++ src/main/java/com/example/cocoblue/service/JoinRoomService.java
@@ -5,6 +5,7 @@ import com.example.cocoblue.domain.Room;
 import com.example.cocoblue.dto.JoinedMemberList;
 import com.example.cocoblue.repository.RoomRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -12,12 +13,14 @@ import java.util.List;
 import java.util.UUID;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class JoinRoomService {
 
     private final RoomRepository roomRepository;
 
     public JoinedMemberList joinRoom(UUID roomId, String name) {
+        log.info(roomId.toString(), name);
         Room room = roomRepository.getRoom(roomId);
 
         if(room.getMembers().containsKey(name)) {
@@ -33,6 +36,8 @@ public class JoinRoomService {
 
         room.getMembers().put(member.getName(), member);
         List<Member> memberList = new ArrayList<>(room.getMembers().values());
+
+        log.info(memberList.get(0).getName());
 
         return JoinedMemberList.builder()
                 .ownerName(room.getOwnerName())
