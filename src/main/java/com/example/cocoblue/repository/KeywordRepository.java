b1889105caffa78669e6646c840ea617b30b2ff1
jangwooooo <s22018@gsm.hs.kr>
2023-07-28 09:49:54 +0900

    Fix getKeyword to redis

--- src/main/java/com/example/cocoblue/repository/KeywordRepository.java
+++ src/main/java/com/example/cocoblue/repository/KeywordRepository.java
@@ -21,8 +21,7 @@ public class KeywordRepository {
     }
 
     public List<String> popRandomMemberFromSet(String key) {
-        return List.of("사과", "오렌지", "포도", "수박");
-//        return setOperations.pop(key, 4L);
+        return setOperations.pop(key, 4L);
     }
 
     public void storeUnionOfSets(String unionSetKey, String... sourceSetKeys) {
